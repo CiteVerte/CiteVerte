37e62fc6ec1b812ecb76aa1fc4515490d827455b
Kaela M Gallo <kaelamgallo@Kaelas-MBP.home>
2020-02-26 00:07:49 -0500

    did a basic layout for the homapage- will work on nav tmrr

---
+++ citeverte/src/Components/HomePage.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="Container">
+      <div className="Body">
+        <div className="Main-Section">
+          <div className="section-title">
+            <p>section title</p>
+          </div>
+          <div className="section-body">
+            <div className="section-image">image</div>
+            <div className="section-text">
+              <p> some text</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="Sectionsmall">
+          <div className="section-title">
+            <p>section title</p>
+          </div>
+          <div className="section-body">
+            <div className="section-image"></div>
+            <div className="section-text"></div>
+          </div>
+        </div>
+
+        <div className="Section">
+          <div className="section-title">
+            <p>section title</p>
+          </div>
+          <div className="section-body">
+            <div className="section-image"></div>
+            <div className="section-text"></div>
+          </div>
+        </div>
+
+        <div className="Section">
+          <div className="section-title">
+            <p>section title</p>
+          </div>
+          <div className="section-body">
+            <div className="section-image"></div>
+            <div className="section-text"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
